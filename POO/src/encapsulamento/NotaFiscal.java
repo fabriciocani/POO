5afdd9c0e9cfd4e7894becd8ca531a41083c5d89
crfranco <crfranco@crfranco.laptop>
2015-03-20 19:45:29 -0300

    teste

---
+++ POO/src/encapsulamento/NotaFiscal.java
@@ -0,0 +1,13 @@
+package encapsulamento;
+
+public class NotaFiscal {
+	
+	private int numero;
+	private String data;
+	
+	private String vendedor // nome, cpf;
+	private String[] produtos //codigo descricao e preco;
+	
+	
+
+}
